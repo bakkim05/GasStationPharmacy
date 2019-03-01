e742674c21ced0dedd27cbd84975d79a40f75ec8
Dagoberto Rojas <gutito96@gmail.com>
2019-03-01 11:23:40 -0600

    update

---
+++ GasStationPharmacy/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GasStationPharmacy.Models
+{
+    public class UserModel
+    {
+        private int id;
+        public int Id { get; set; }
+        private string userName;
+        public string UserName { get; set; }
+        private string Password { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Cedula { get; set; }
+        private string FechaNacimiento { get; set; }
+        private string Phone { get; set; }
+        private string Dir { get; set; }
+        private string Token { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+            UserModel m1 = new UserModel();
+            m1.
+        }
+    }
+}
